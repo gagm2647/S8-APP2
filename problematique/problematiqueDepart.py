fd4677886e691cec3247b53ff1483f365ab27e58
Mathieu Gagnon <gagm2647@usherbrooke.ca>
2022-11-01 17:54:40 -0400

    kmean at ish 69%

--- problematique/problematiqueDepart.py
+++ problematique/problematiqueDepart.py
@@ -248,6 +248,7 @@ def main():
         forests_features = forests_features / np.max(forests_features, axis=0)
         streets_features = streets_features / np.max(streets_features, axis=0)
         features = features  / np.max(features, axis=0)
+        
         min, max = np.min(features), np.max(features)
         donneesTest = an.genDonneesTest(ndonnees, an.Extent(xmin=min, xmax=max, ymin=min, ymax=max), ndim=features.shape[1])
 
@@ -255,8 +256,8 @@ def main():
 
         # x-mean sur chacune des classes
         # suivi d'un y-PPV avec ces nouveaux représentants de classes
-        cluster_centers, cluster_labels = classifiers.full_kmean(25, data, labels, 'kmean', an.Extent(xmin=min, xmax=max, ymin=min, ymax=max))
-        classifiers.full_ppv(25, cluster_centers, cluster_labels, donneesTest, '5v5', an.Extent(xmin=min, xmax=max, ymin=min, ymax=max), features, labels)
+        cluster_centers, cluster_labels = classifiers.full_kmean(150, data, labels, 'kmean', an.Extent(xmin=min, xmax=max, ymin=min, ymax=max))
+        classifiers.full_ppv(1, cluster_centers, cluster_labels, donneesTest, '5v5', an.Extent(xmin=min, xmax=max, ymin=min, ymax=max), features, labels)
 
     if False:
         features = np.zeros((len(coasts_features)+len(forests_features)+len(streets_features),features.shape[1]))
